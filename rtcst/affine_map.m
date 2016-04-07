650a180292db0401f9bbd6bce89e4a2f6bed156a
Sizhuo Ma <sizhuoma@gmail.com>
2016-04-06 21:30:15 -0500

    Implemented affine_map() and initialize_templates()

--- rtcst/affine_map.m
+++ rtcst/affine_map.m
@@ -7,7 +7,10 @@ function [ y ] = affine_map( F, s, b )
 %Output:
 %   y: mapped observation, size d0=w*h
 
-d0 = b(3) * b(4);
+tform = affine2d([s(1) s(2) 0; s(3) s(4) 0; s(5) s(6) 1]);
+Fw = imwarp(F, invert(tform));
+y = Fw(b(1):b(1)+b(3)-1, b(2):b(2)+b(4)-1);
+y = y(:);
 
 end
 
